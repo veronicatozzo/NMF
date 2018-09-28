3eb1567108b9d4afcdf5497277129810d0321a1c
Unknown <veronica.tozzo@gmail.com>
2018-09-28 15:19:50 +0100

    added max cores

--- nmtf/scripts/cross_val.py
+++ nmtf/scripts/cross_val.py
@@ -2,6 +2,10 @@
 import sys
 
 import pickle as pkl
+import os
+os.environ["MKL_NUM_THREADS"] = "8"
+os.environ["NUMEXPR_NUM_THREADS"] = "8"
+os.environ["OMP_NUM_THREADS"] = "8"
 
 from os import listdir
 from os.path import isfile, join
@@ -9,7 +13,7 @@ from os.path import isfile, join
 from nmtf.nmtf import SSNMTF_CV
 from nmtf.read import get_adjacency
 
-parent_folder = "/cs/research/bioinf/bionet1/Coexpression_Study/BCStages-SubtypesNetworks/
+parent_folder = "/cs/research/bioinf/bionet1/Coexpression_Study/BCStages-SubtypesNetworks/"
 folders = ["HER2Networks", "LuminalBNetworks", "Stage2Networks",
            "Stage4Networks", "LuminalANetworks",  "Stage1Networks",
            "Stage3Networks",  "TripleNegativeNetworks"]
@@ -19,7 +23,7 @@ for i, fold in enumerate(folders):
     complete_path = parent_folder + fold
     files = [join(complete_path, f) for f in listdir(complete_path)
              if isfile(join(complete_path, f))]
-    print("... for a total of %d networks.." % len(files).)
+    print("... for a total of %d networks.." % len(files))
     print("Getting adjacency matrices..")
     graphs = [get_adjacency(f) for f in files]
     print("Starting cross_validation")
